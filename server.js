62fae7b8f9427ef64802ce97fcfc5784138a1d07
Roy Hasson <rhasson@gmail.com>
2012-10-18 00:31:56 -0400

    added binaryjs streams

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+var fs = require('fs');
 var e = require('express');
 var s = e();
+var BServer = require('binaryjs').BinaryServer;
+var bs = BServer({port: 9000});
 
 s.use(e.static(__dirname));
 
 s.listen(8080, 'localhost');
+
+bs.on('connection', function(client) {
+	console.log('connected', client);
+	client.on('error', function(err) {
+		console.log('error: ', e.stack, e.message);
+	});
+
+	var count = 0;
+	client.on('stream', function(stream, meta) {
+		if (meta.type === 'write') {
+			count++;
+			//var f = fs.createWriteStream(__dirname + '/pic'+count+'.png');
+			//stream.pipe(f);
+			console.log(stream);
+		}
+	});
+});
+
